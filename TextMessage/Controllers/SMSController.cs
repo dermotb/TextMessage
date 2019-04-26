05e1365967389552a67e13c3e90808ad434b2e47
Dermot Boyle <dermot.boyle@ellucian.com>
2019-04-26 12:40:24 +0100

    Add project files.

---
+++ TextMessage/Controllers/SMSController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TextMessage.Models;
+
+namespace TextMessage.Controllers
+{
+    public class SMSController : Controller
+    {
+        // GET: SMS
+        public ActionResult SentMessage(Message msg)
+        {
+            return View(msg);
+        }
+
+
+        // GET: SMS/Create
+        public ActionResult SendMessage()
+        {
+            return View();
+        }
+
+        // POST: SMS/Create
+        [HttpPost]
+        public ActionResult SendMessage(Message msg)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    return RedirectToAction("SentMessage", msg);
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch (Exception ex)
+            {
+                return new HttpStatusCodeResult(500, ex.Message);
+            }
+        }
+
+    
+    }
+}
---
+++ TextMessage/Models/Message.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TextMessage.Models
+{
+    public class Message
+    {
+        [Required]
+        [Phone]
+        [Display(Name = "Destination Number:")]
+        public string DestinationNumber { get; set; }
+        [MinLength(1)]
+        [MaxLength(140)]
+        [Display(Name = "Message text:")]
+        public string Content { get; set; }
+    }
+}
