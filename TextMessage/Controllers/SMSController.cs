5e52e7630e97e9c3ab3b5ce8e3b153c62662fdcb
Dermot Boyle <dermotb@hotmail.com>
2022-03-31 21:20:58 +0100

    Add project files.

---
+++ TextMessage/Controllers/SMSController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TextMessage.Models;
+
+namespace TextMessage.Controllers
+{
+    public class SMSController : Controller
+    {
+         // GET: SMSController/Details/5
+        public ActionResult Confirm(SMSMessage msg)
+        {
+            return View(msg);
+        }
+
+        // GET: SMSController/Create
+        public ActionResult Send()
+        {
+            return View();
+        }
+
+        // POST: SMSController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Send(SMSMessage msg)
+        {
+            try
+            {
+                return RedirectToAction("Confirm", msg);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+    }
+}
---
+++ TextMessage/Models/SMSMessage.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TextMessage.Models
+{
+    public class SMSMessage
+    {
+        [Required(ErrorMessage = "Need a number!")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "Phone Number must be 10 digits")]
+        [DisplayName("To")]
+        public string ToMobileNumber { get; set; }
+
+        [Required(ErrorMessage = "Message must not be blank!")]
+        [StringLength(140, ErrorMessage = "Maximum message is 140 characters!")]
+        [DisplayName("Message")]
+        public string Content { get; set; }
+    }
+}
